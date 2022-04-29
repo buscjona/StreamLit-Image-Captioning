1e72dbbeb3f9c97e73902b138f7685142e6cf63c
Jonas Busch <92538801+buscjona@users.noreply.github.com>
2022-04-29 20:59:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-st.title('Image Captioning')
+st.title("Image Caption Generator")
+st.text("Hello, we are Jonas, Moritz and Ole and our goal is to build, explain and present")
+st.text("an image caption generator using Deep Learning and Neural Networks. We use these")
+st.text("two subtypes of machine learning, as the combination of the two is closest to ")
+st.text("the way humans analyze images.")
+st.text("")
+
+data_load_state = st.subheader("Loading data...")
+data = pd.read_csv("sample_data_oneMill.csv")
+data_load_state.subheader("Loading data...done!")
