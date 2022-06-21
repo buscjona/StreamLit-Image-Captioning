9fa90ed961a46be8f3297be8f79783585f0888de
ok1341 <ole2809@gmail.com>
2022-06-21 11:59:20 +0200

    Delete data subsets directory

--- data subsets/jsonToCSV.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_json("cars.json")
-df2 = pd.read_json("chairs.json")
-df3 = pd.read_json("couch.json")
-
-df = pd.concat([df1, df2, df3])
-
-df.to_csv("subsets.csv", index=False)
